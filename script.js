c8dfc722e9a5e71f3d4018f23803c98cc4141a7b
Justas94 <jaskunas.justas@gmail.com>
2021-11-27 20:41:41 +0000

    Update script.js
    Comments for the game

--- script.js
+++ script.js
@@ -20,10 +20,15 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔️ No number! Try Again";
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too High! Try Again";
@@ -33,6 +38,7 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "😕 You lost the game!";
       document.querySelector(".score").textContent = 0;
     }
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📉 Too Low! Try Again";
